623ef834ebe96e55b6e67b5f2aa460646bd4029f
zhangxin <heermosi39@163.com>
2019-06-30 03:21:04 +0800

    feat(tool入口): 0.0.1构建,tool entry
    [release] 0.0.1

--- publish.sh
+++ publish.sh
@@ -7,5 +7,13 @@ echo  # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Releasing $VERSION ..."
+
+  # commit
+  git add -A
+  npm run commit
+  npm version $VERSION --message "[release] $VERSION"
+  git push origin master
+
+  # publish
   npm publish
 fi
